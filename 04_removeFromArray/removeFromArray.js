80e1ecd7ec21718a46a1bded9b48f514d13ea53b
valueinit <138997978+valueinit@users.noreply.github.com>
2023-07-29 20:54:02 +0200

    all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function( arrayInput , ...toBeRemoveds ) {
+    const arrayOutput = [];
+
+
+    for (let i = 0; i < arrayInput.length; i++){
+        if (toBeRemoveds.includes(arrayInput[i]) == false){
+            arrayOutput.push(arrayInput[i]);
+        }
+        
+    }
+
+    return arrayOutput;
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+let x = removeFromArray([1, 2, 3, 4], 3);
