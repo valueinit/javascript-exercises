b48cfb941b0411c6774039499dd801e97deded7f
valueinit <138997978+valueinit@users.noreply.github.com>
2023-07-29 18:58:45 +0200

    all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(wordForward) {
+    const wordArrayBackward = [''];
+    const wordArrayForward = Array.from(wordForward);
+    const wordArrayLength = wordArrayForward.length;
 
+    for (let i = 0 ; i < (wordArrayLength + 1) ; i++){
+        wordArrayBackward.push(wordArrayForward[wordArrayLength - i]);
+    }
+
+    const wordBackward = wordArrayBackward.join('');
+    return wordBackward;
 };
 
 // Do not edit below this line
