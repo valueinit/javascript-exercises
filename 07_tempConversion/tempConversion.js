58be18bea154374de0ce7a01c1dd76b19771d7e7
valueinit <138997978+valueinit@users.noreply.github.com>
2023-07-29 22:34:37 +0200

    all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function makeDecimal(nonDecimalNumber){
+  return Number(nonDecimalNumber.toFixed(1));
+}
+
+const convertToCelsius = function(tempInFahrenheit) {
+  return makeDecimal((tempInFahrenheit - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  return makeDecimal(tempInCelsius * 9 / 5 + 32);
 };
 
 // Do not edit below this line
