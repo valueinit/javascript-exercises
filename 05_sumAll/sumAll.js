2a76fd0f0a72eb76f7824bd5e16ceb2c9e495661
valueinit <138997978+valueinit@users.noreply.github.com>
2023-07-29 21:43:56 +0200

    all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+function isInvalidNumber(value){
+    return typeof value != "number" || value < 0;
+}
 
+const sumAll = function(number1, number2) {
+    
+    if(isInvalidNumber(number1) || isInvalidNumber(number2)){
+        return "ERROR";
+    }
+  
+    let smallerNumber;
+    let biggerNumber;
+    
+    if(number1>number2){
+        smallerNumber = number2;
+        biggerNumber = number1;
+    }else{
+        smallerNumber = number1;
+        biggerNumber = number2;
+    }
+
+    let summe = 0;
+    for (let i = smallerNumber ; i <= biggerNumber; i++){
+        summe += i; 
+    }
+    return summe;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+let x = sumAll(-2,4);
