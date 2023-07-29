2adac6a5514e33ed15c20a654782022f3c029b9e
valueinit <138997978+valueinit@users.noreply.github.com>
2023-07-29 22:11:46 +0200

    all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+function isDivisable(divident, divisor){
+    return (divident % divisor == 0);  
+}
 
+
+const leapYears = function(inputYear) {
+
+    return  ((isDivisable(inputYear, 100) && isDivisable(inputYear, 400)) ||
+            (isDivisable(inputYear,4) && !isDivisable(inputYear, 100)));
 };
 
 // Do not edit below this line
